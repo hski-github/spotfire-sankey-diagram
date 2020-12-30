c37530a2fc26baa7d0b8f55822d341b753c67416
hski <holger@schimanski-web.de>
2020-12-30 16:36:44 +0100

    Simplification of code by using await

--- src/main.js
+++ src/main.js
@@ -50,30 +50,26 @@ Spotfire.initialize(async (mod) => {
         /**
          * Print out to document
          */
-		google.charts.load('current', {'packages':['sankey']});
-		google.charts.setOnLoadCallback(drawChart);
+		await google.charts.load('current', {'packages':['sankey']});
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'From');
+		data.addColumn('string', 'To');
+		data.addColumn('number', 'Weight');
+		data.addRows([
+	      [ 'A', 'X', 5 ],
+	      [ 'A', 'Y', 7 ],
+	      [ 'A', 'Z', 6 ],
+	      [ 'B', 'X', 2 ],
+	      [ 'B', 'Y', 9 ],
+	      [ 'B', 'Z', 4 ]
+		]);
 
-		function drawChart() {
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'From');
-			data.addColumn('string', 'To');
-			data.addColumn('number', 'Weight');
-			data.addRows([
-		      [ 'A', 'X', 5 ],
-		      [ 'A', 'Y', 7 ],
-		      [ 'A', 'Z', 6 ],
-		      [ 'B', 'X', 2 ],
-		      [ 'B', 'Y', 9 ],
-		      [ 'B', 'Z', 4 ]
-			]);
+		// Sets chart options.
+		var options = { width: 600, height: 200 };
 
-			// Sets chart options.
-			var options = { width: 600, height: 200 };
-
-			// Instantiates and draws our chart, passing in some options.
-			var chart = new google.visualization.Sankey(document.getElementById('mod-container'));
-			chart.draw(data, options);
-		}
+		// Instantiates and draws our chart, passing in some options.
+		var chart = new google.visualization.Sankey(document.getElementById('mod-container'));
+		chart.draw(data, options);
 
         /**
          * Signal that the mod is ready for export.
